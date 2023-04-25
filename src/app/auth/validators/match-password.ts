5b852c604fc69ace6fb1db1fac347052b3ba0162
tagaism <i77tag@gmail.com>
2023-04-26 00:05:27 +0900

    fix formControl incapability bug on custom validation.

--- src/app/auth/validators/match-password.ts
+++ src/app/auth/validators/match-password.ts
@@ -1,17 +1,25 @@
 import { Injectable } from "@angular/core";
-import { FormGroup, ValidationErrors, Validator } from "@angular/forms";
+import { AbstractControl, ValidationErrors, Validator } from "@angular/forms";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MatchPassword implements Validator{
-  
-  validate(control: FormGroup): ValidationErrors | null {
-    const { password, passwordConfirmation} = control.value;
-    if(password === passwordConfirmation) {
+  validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    const { password, passwordConfirmation } = control.value;
+    if(password === passwordConfirmation){
       return null;
     }
-    return { PasswordsDoNotMatch: true };
+    return { PasswordDontMatch: true };
   }
+
+
+  // validate(control: Ab): ValidationErrors | null {
+  //   const { password, passwordConfirmation} = control.value;
+  //   if(password === passwordConfirmation) {
+  //     return null;
+  //   }
+  //   return { PasswordsDoNotMatch: true };
+  // }
 
 }
