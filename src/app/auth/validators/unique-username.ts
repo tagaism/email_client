1a010cf6b7abc2b305d7396e054027a497ee5d0a
tagaism <i77tag@gmail.com>
2023-04-26 23:58:04 +0900

    started unique user validation.

---
+++ src/app/auth/validators/unique-username.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { AbstractControl, AsyncValidator, ValidationErrors } from "@angular/forms";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UniqueUsername implements AsyncValidator{
+
+  constructor(
+    private http:HttpClient
+  ){}
+
+  validate(control: AbstractControl<any, any>): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    const { value } = control;
+
+    return this.http.post<any>('https://api.angular-email.com/auth/username', {
+      username: value
+    })
+    // return { UserNameTaken: true }
+  }
+}
