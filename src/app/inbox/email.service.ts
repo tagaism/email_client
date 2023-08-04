6a54230453e21dc72a2b85b1abdad784d94d31de
tagaism <i77tag@gmail.com>
2023-08-04 23:59:02 +0900

    Email pages

---
+++ src/app/inbox/email.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface EmailSummary {
+  id: string,
+  title: string,
+  from: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+  rootUrl = "https://api.angular-email.com/"
+
+  constructor(
+    private http: HttpClient;
+  ) { }
+  
+  getEmails() {
+    this.http.get<EmailSummary[]>(`${this.rootUrl}/emails`)
+  }
+}
--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { InboxRoutingModule } from './inbox-routing.module';
 import { HomeComponent } from './home/home.component';
+import { EmailShowComponent } from './email-show/email-show.component';
+import { EmailCreateComponent } from './email-create/email-create.component';
+import { EmailReplyComponent } from './email-reply/email-reply.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    EmailShowComponent,
+    EmailCreateComponent,
+    EmailReplyComponent
   ],
   imports: [
     CommonModule,
