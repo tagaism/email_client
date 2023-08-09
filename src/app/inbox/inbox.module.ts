d12812f8368b18b853b498e78e0319da1d5d0922
tagaism <i77tag@gmail.com>
2023-08-09 22:04:46 +0900

    Modal boilerplate

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -9,7 +9,7 @@ import { EmailReplyComponent } from './email-reply/email-reply.component';
 import { EmailIndexComponent } from './email-index/email-index.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -23,7 +23,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     CommonModule,
-    InboxRoutingModule
+    InboxRoutingModule,
+    SharedModule
   ]
 })
 export class InboxModule { }
